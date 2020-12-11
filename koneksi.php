98082358df7eb934398c38cde4084b0ca528c670
viki179 <71555821+viki179@users.noreply.github.com>
2020-12-11 10:28:23 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
    $hostname  = "localhost";
    $username  = "root";
-   $password  = "";
+   $password  = ""; 
    $dbname  = "latihan";
    $db = new mysqli($hostname, $username, $password, $dbname);
 ?>
